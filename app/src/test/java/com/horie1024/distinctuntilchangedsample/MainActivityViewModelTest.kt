13bf8fc38dbaf68ea4a2bbca945790c065660422
horie1024 <ryosuke.pic@gmail.com>
2018-12-23 17:13:15 +0900

    Using MockK.

--- app/src/test/java/com/horie1024/distinctuntilchangedsample/MainActivityViewModelTest.kt
+++ app/src/test/java/com/horie1024/distinctuntilchangedsample/MainActivityViewModelTest.kt
@@ -1,18 +1,14 @@
 package com.horie1024.distinctuntilchangedsample
 
-import android.util.Log
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth.assertThat
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito.times
-import org.mockito.Mockito.verify
-import org.mockito.MockitoAnnotations
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityViewModelTest {
@@ -21,23 +17,31 @@ class MainActivityViewModelTest {
     @JvmField
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @Mock
-    lateinit var observer: Observer<Int>
+    private lateinit var viewModel: MainActivityViewModel
 
     @Before
     fun setUp() {
-        MockitoAnnotations.initMocks(this)
+        viewModel = MainActivityViewModel()
     }
 
     @Test
     fun distinctUntilChangedの挙動確認() {
 
-        val viewModel = MainActivityViewModel()
+        val mock = mockk<Observer<Int>>(relaxed = true)
 
-        viewModel.value.observeForever(observer)
+        viewModel.value.observeForever(mock)
         viewModel.setValue(1)
         viewModel.setValue(1)
 
-        verify(observer, times(1)).onChanged(1)
+        verify(exactly = 1) {
+            mock.onChanged(1)
+        }
+
+        viewModel.setValue(2)
+        viewModel.setValue(1)
+
+        verify(exactly = 2) {
+            mock.onChanged(1)
+        }
     }
 }
